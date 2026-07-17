c2a02f7af5a84f16432183929332e4564f22daee
vanshikas03 <vanshikas@aaysinsight.com>
2026-07-18 00:43:45 +0530

    Fix ESLint errors

--- src/components/Abc/LandingPage.jsx
+++ src/components/Abc/LandingPage.jsx
@@ -1,25 +1,35 @@
 import React from "react";
-import { Button } from "../Layout/Button";
 import data from "../../data/data.json";
+
 const LandingPage = ({ name }) => {
-  const [dashboardData, setDashboardData] = React.useState(data);
+  const [dashboardData] = React.useState(data);
+
   console.log(dashboardData);
+
   return (
-   <div>
-    <span>
-      <p>Average Assessment Score: {dashboardData.overview.average_assessment_score}</p>
-      <p> Average Feedback Score: {dashboardData.overview.average_feedback_score}</p>
-    </span>
-    <span className = "text-red-500">
+    <div>
+      <span>
+        <p>
+          Average Assessment Score:{" "}
+          {dashboardData.overview.average_assessment_score}
+        </p>
+
+        <p>
+          Average Feedback Score:{" "}
+          {dashboardData.overview.average_feedback_score}
+        </p>
+      </span>
+
+      <span className="text-red-500">
         {dashboardData?.monthly_trend?.map((item, index) => (
           <div key={index}>
-            <p>attendees : {item?.attendees}</p>
-            <p>Month Label : {item?.month_label}</p>
+            <p>Attendees: {item?.attendees}</p>
+            <p>Month Label: {item?.month_label}</p>
           </div>
         ))}
       </span>
-   </div>
+    </div>
   );
 };
- 
+
 export default LandingPage;
